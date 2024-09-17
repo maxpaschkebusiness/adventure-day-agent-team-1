1f186a2bd07d7a46d81504f3d9979b4525d45d72
maxpaschkebusiness <128611175+maxpaschkebusiness@users.noreply.github.com>
2024-09-17 09:08:57 +0000

    with print

--- src-agents/phase1/main.py
+++ src-agents/phase1/main.py
@@ -10,16 +10,19 @@ app = FastAPI()
 
 load_dotenv()
 
+
 class QuestionType(str, Enum):
     multiple_choice = "multiple_choice"
     true_false = "true_or_false"
     popular_choice = "popular_choice"
     estimation = "estimation"
 
+
 class Ask(BaseModel):
     question: str | None = None
     type: QuestionType
     correlationToken: str | None = None
+
 
 class Answer(BaseModel):
     answer: str
@@ -27,41 +30,53 @@ class Answer(BaseModel):
     promptTokensUsed: int | None = None
     completionTokensUsed: int | None = None
 
+
 client: AzureOpenAI
 
 if "AZURE_OPENAI_API_KEY" in os.environ:
     client = AzureOpenAI(
-        api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
-        api_version = os.getenv("AZURE_OPENAI_VERSION"),
-        azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        api_version=os.getenv("AZURE_OPENAI_VERSION"),
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
     )
 else:
-    token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
+    token_provider = get_bearer_token_provider(
+        DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
     client = AzureOpenAI(
         azure_ad_token_provider=token_provider,
-        api_version = os.getenv("AZURE_OPENAI_VERSION"),
-        azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_version=os.getenv("AZURE_OPENAI_VERSION"),
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
     )
 
 deployment_name = os.getenv("AZURE_OPENAI_COMPLETION_DEPLOYMENT_NAME")
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello Smoorghs"}
 
-@app.post("/ask", summary="Ask a question", operation_id="ask") 
+
+@app.post("/ask", summary="Ask a question", operation_id="ask")
 async def ask_question(ask: Ask):
-    """
-    Ask a question
-    """
+    # """
+    # # Ask a question
+    # """
 
     # Send a completion call to generate an answer
     print('Sending a request to openai')
-    start_phrase =  ask.question
+
+    start_phrase = ask.question
+    print("Question:    "+start_phrase)
     response: openai.types.chat.chat_completion.ChatCompletion = None
 
-    # Send a completion call to generate an answer
+    response = client.chat.completions.create(
+        model=deployment_name,
+        messages=[{"role": "assistant", "content": "I'm a 5 year old and i have the following question. Please answer only in short answers:" + start_phrase},
+                  {"role": "system", "content": "Answer this question with content only."}]
+    )
 
+    print(response.choices[0].message.content)
+    print(response)
     answer = Answer(answer=response.choices[0].message.content)
     answer.correlationToken = ask.correlationToken
     answer.promptTokensUsed = response.usage.prompt_tokens
